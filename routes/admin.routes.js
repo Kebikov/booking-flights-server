b0f0d24a08945f2c1ac42ee547fd39609aa001b7
Gen <kebikovgen@gmail.com>
2023-11-16 16:06:13 +0300

    added route postFormFlights

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -9,7 +9,6 @@ router.get('/curent-time', async (req, res) => {
     try {
         const date = new Date();
         const dataTime = new Date(date).getTime();
-
         return res.status(200).send({time: dataTime});
     }catch (error) {
         res.status(500).json({message: `Ошибка сервера, попробуйте позже...${error}`});
--- routes/sql.routes.js
+++ routes/sql.routes.js
@@ -5,9 +5,13 @@ const router = express.Router({mergeParams: true});
 const getBookingData = require('../controllers/getBookingData');
 const getFlightsData = require('../controllers/getFlightsData');
 
+const postFormFlights = require('../controllers/postFormFlights');
 
+//= GET 
 router.get('/booking-data', getBookingData);
 router.get('/flights-data', getFlightsData);
+//= POST 
+router.post('/check-form-flights', postFormFlights);
 
 
 module.exports = router;
